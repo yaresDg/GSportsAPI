3ef7bc5161f899009579c5a6abdc06a29dc27a14
Yares <yaresmw@gmail.com>
2025-10-28 11:27:09 -0300

    compression

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import compression from 'compression';
 import indexRoutes from './Routes/indexRoutes.js';
 
 import dotenv from 'dotenv';
@@ -15,6 +16,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(compression());
 app.use('/',indexRoutes);
 
 app.listen(port,()=>{
