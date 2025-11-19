3661a2cac5b8f6373688348ebbefdd85ded8a3d7
Yares <yaresmw@gmail.com>
2025-11-19 10:27:19 -0300

    modelo para radios en mongoDb

---
+++ model/radioModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const RadioSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true
+    },
+    nombre: {
+      type: String,
+      required: true
+    },
+    descripcion: {
+      type: String,
+      default: ""
+    },
+    pais: {
+      type: String,
+      required: true
+    },
+    id_thesportsdb: {
+      type: [String],
+      default: []
+    },
+    streams: {
+      type: [String],
+      default: []
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Radio = mongoose.model("Radio", RadioSchema);
+
+export default Radio;
